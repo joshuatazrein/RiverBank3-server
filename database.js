cd25d9b0a756a4d1031fb2dc89f3d6b01430ef70
Joshua Tazman Reinier <84872947+joshuatazrein@users.noreply.github.com>
2022-02-21 13:48:23 -0800

    fixed module exports error

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ const jsonfile = require('jsonfile');
 
 const path = 'data/'; // path to get data files
 
-export function readFile(username, password) {
+function readFile(username, password) {
   const data = jsonfile.readFileSync(path + username + password + '.json');
   return data;
 }
@@ -11,21 +11,21 @@ function writeFile(username, password, data) {
   jsonfile.writeFileSync(path + username + password + '.json', data);
 }
 
-export function updateTaskDataFile(username, password, id, value) {
+function updateTaskDataFile(username, password, id, value) {
   // update a single tasks's data
   const data = jsonfile.readFileSync(username, password);
   data.tasks[id] = value;
   writeFile(username, password, data);
 }
 
-export function removeTaskDataFile(username, password, id) {
+function removeTaskDataFile(username, password, id) {
   // remove a single tasks' data
   const data = jsonfile.readFileSync(username, password);
   delete data.tasks[id];
   writeFile(username, password, data);
 }
 
-export function uploadSettingsFile(username, password, data) {
+function uploadSettingsFile(username, password, data) {
   // update the settings of a JSON file
   const fullData = jsonfile.readFileSync(username, password);
   fullData.settings = data;
@@ -33,9 +33,17 @@ export function uploadSettingsFile(username, password, data) {
   
 }
 
-export function uploadTasksFile(username, password, data) {
+function uploadTasksFile(username, password, data) {
   // update the tasks of a JSON file
   const fullData = jsonfile.readFileSync(username, password);
   fullData.tasks = data;
   writeFile(username, password, data);
+}
+
+module.exports = {
+  readFile, 
+  updateTaskDataFile, 
+  removeTaskDataFile, 
+  uploadSettingsFile,
+  uploadTasksFile
 }
